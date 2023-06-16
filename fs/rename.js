d3e6696efd16d7c7518b8d929fed04555cd41e24
orifjonov-abbosbek <zloy19781999@gmail.com>
2023-06-16 19:06:34 +0500

    rename solved

--- fs/rename.js
+++ fs/rename.js
@@ -1,5 +1,29 @@
-const rename = async () => {
-    // Write your code here 
+import { rename, access } from "node:fs/promises";
+
+const renameFile = async () => {
+  const sourceFile = "./files/wrongFilename.txt";
+  const changedFile = "./files/properFilename.md";
+
+  try {
+    await access(sourceFile);
+    try {
+      await access(changedFile);
+      throw new Error("FS operation failed.");
+    } catch (err) {
+      if (err.code === "ENOENT") {
+        await rename(sourceFile, changedFile);
+        console.log("File renamed successfully.");
+      } else {
+        throw err;
+      }
+    }
+  } catch (err) {
+    throw new Error("FS operation failed.");
+  }
 };
 
-await rename();
+try {
+  await renameFile();
+} catch (err) {
+  console.error(err.message);
+}
