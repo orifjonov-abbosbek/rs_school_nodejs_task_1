6681b3ace54fcbc9b0ec0cc1bc07a4549d4cbd2b
orifjonov-abbosbek <zloy19781999@gmail.com>
2023-06-16 19:12:09 +0500

    delete solved

--- fs/delete.js
+++ fs/delete.js
@@ -1,5 +1,14 @@
+import { unlink } from "node:fs/promises";
+
 const remove = async () => {
-    // Write your code here 
+  const deleteFile = "./files/fileToRemove.txt";
+
+  try {
+    await unlink(deleteFile);
+    console.log("File successfully deleted");
+  } catch (err) {
+    console.error("FS operation failed", err.message);
+  }
 };
 
 await remove();
