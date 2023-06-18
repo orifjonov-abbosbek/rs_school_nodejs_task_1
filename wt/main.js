236d6e02d52187b34e56efd1763e93163dcc3427
orifjonov-abbosbek <zloy19781999@gmail.com>
2023-06-18 06:17:53 +0500

    wt completed

--- wt/main.js
+++ wt/main.js
@@ -1,5 +1,31 @@
+import { Worker } from "worker_threads";
+import os from "os";
+
 const performCalculations = async () => {
-    // Write your code here
+  let number = 10;
+  const numCores = os.cpus().length;
+const workerFile = new URL("./worker.js", import.meta.url);
+
+  const workersData = await Promise.allSettled(
+    Array.from({ length: numCores }, () => {
+      return new Promise((resolve, reject) => {
+        const worker = new Worker(workerFile, { workerData: number++ });
+
+        worker.on("message", (message) => resolve(message));
+        worker.on("error", (error) => reject(error));
+      });
+    })
+  );
+
+  const results = workersData.map((item) => {
+    if (item.status === "fulfilled") {
+      return { status: "resolved", data: item.value };
+    } else {
+      return { status: "error", data: item.reason };
+    }
+  });
+
+  console.log(results);
 };
 
-await performCalculations();
+performCalculations().catch((error) => console.error(error));
