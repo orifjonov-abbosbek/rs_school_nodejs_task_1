115e588a7d2c2b32b82e20f746e42359f9e7ad2a
orifjonov-abbosbek <zloy19781999@gmail.com>
2023-06-18 06:57:00 +0500

    cp complated

--- cp/cp.js
+++ cp/cp.js
@@ -1,6 +1,17 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { spawn } from "child_process";
+
+const spawnChildProcess = () => {
+  const childProcess = spawn("node", ["-v"]);
+
+  process.stdin.pipe(childProcess.stdin);
+
+  childProcess.stdout.on("data", (data) => {
+    process.stdout.write(`Received from child process: ${data}`);
+  });
+
+  childProcess.on("exit", (code) => {
+    console.log(`Child process exited with code ${code}`);
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess();
