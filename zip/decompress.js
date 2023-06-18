366c5df6ccbca64b7f04709ceed69200bd53a2b3
orifjonov-abbosbek <zloy19781999@gmail.com>
2023-06-18 05:06:39 +0500

    zlib completed

--- zip/decompress.js
+++ zip/decompress.js
@@ -1,5 +1,22 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { createUnzip } from "node:zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  const zipFile = "./files/archive.gz";
+  const decompressFile = "./files/fileToCompress.txt";
+
+  const archive = createReadStream(zipFile);
+  const decompressedFile = createWriteStream(decompressFile);
+
+  const decompression = createUnzip();
+
+  await new Promise((resolve, reject) => {
+    archive
+      .pipe(decompression)
+      .pipe(decompressedFile)
+      .on("finish", resolve(console.log("file decompressed successfully")))
+      .on("error", reject(console.log("error while decompression")));
+  });
 };
 
 await decompress();
